d6d8bd6b5f250109ed9993e9998493bdfe1f0ff0
Stéphane Charron <stephanecharron.web@gmail.com>
2016-09-14 12:50:18 -0400

    Update json-flatten.js

--- json-flatten.js
+++ json-flatten.js
@@ -21,3 +21,22 @@ JSON.flatten = function(data) {
     recurse(data, "");
     return result;
 }
+
+JSON.unflatten = function(data) {
+    "use strict";
+    if (Object(data) !== data || Array.isArray(data))
+        return data;
+    var regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
+        resultholder = {};
+    for (var p in data) {
+        var cur = resultholder,
+            prop = "",
+            m;
+        while (m = regex.exec(p)) {
+            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+            prop = m[2] || m[1];
+        }
+        cur[prop] = data[p];
+    }
+    return resultholder[""] || resultholder;
+};
